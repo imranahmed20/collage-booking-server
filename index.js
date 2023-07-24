e47d5c117c34bc1ab0f68e78f40dc25d2cef286a
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-07-24 22:48:52 +0600

    last commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const collageCollection = client.db('collageBooking').collection('collage')
         const nameCollection = client.db('collageBooking').collection('name')
